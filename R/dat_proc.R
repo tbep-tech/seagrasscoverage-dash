77e6c26d10b283bb16bc1ea0238ff53b25e2bd94
fawda123 <mbafs2012@gmail.com>
2020-06-21 21:17:02 -0400

    sand and attached algae added to data, app

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -8,7 +8,7 @@ library(foreach)
 prj <- 4326
 
 urls <- list(
-  `2018` = 'https://opendata.arcgis.com/datasets/e7e0a72a33274ca3a3ce39629e2de250_0.geojson',
+  `2018` = 'https://opendata.arcgis.com/datasets/8d0d473468924423bf0f1682aaca790f_0.geojson',
   `2016` = 'https://opendata.arcgis.com/datasets/f0ecff0cf0de491685f8fb074adb278b_20.geojson',
   `2014` = 'https://opendata.arcgis.com/datasets/f530f972ded749adb1c6b20c2651e7f9_18.geojson',
   `2012` = 'https://opendata.arcgis.com/datasets/619bd267e4c54e70968abd86eb92318e_17.geojson',
@@ -39,6 +39,7 @@ res <- foreach(i = 1:nrow(urls), .packages = c('tidyverse', 'sf')) %dopar% {
   print(Sys.time()-strt)
   sink()
   
+  # original in NAD83_HARN_Florida_West_ftUS, transform to wgs84
   prj <- 4326
 
   # import file
@@ -60,7 +61,7 @@ res <- foreach(i = 1:nrow(urls), .packages = c('tidyverse', 'sf')) %dopar% {
   dat_crp <- dat_raw %>%
     st_transform(crs = prj) %>%
     .[segs, ] %>% 
-    filter(FLUCCS_CODE %in% c(9113, 9116)) %>% 
+    filter(FLUCCS_CODE %in% c(9113, 9116, 7210, 9121)) %>% 
     select(OBJECTID, FLUCCS_CODE) %>% 
     st_buffer(dist = 0)
 
@@ -70,22 +71,3 @@ res <- foreach(i = 1:nrow(urls), .packages = c('tidyverse', 'sf')) %dopar% {
   save(list = flnm, file = paste0('data/', flnm, '.RData'), compress = 'xz')
 
 }
-
-# 
-# # 2016 data
-# fl <- 'https://opendata.arcgis.com/datasets/f0ecff0cf0de491685f8fb074adb278b_20.geojson'
-# # import file
-# dat_raw <- st_read(fl)
-# 
-# segs <- st_read('~/Desktop/TBEP/GISboundaries/GISboundaries/TBEP_Bay_Segments_Correct_Projection.shp') %>% 
-#   st_transform(crs = prj)
-# 
-# # 9113 is patchy, 9116 is continuous
-# dat_clp <- dat_raw[segs, ] %>%  
-#   filter(FLUCCS_CODE %in% c(9113, 9116))
-# 
-# stations <- st_as_sf(stations, coords = c('Longitude', 'Latitude'), crs = prj)
-# 
-# mapview(dat_clp) + mapview(stations)
-# 
-# save(dat_clp, file = 'data/dat_clp.RData', compress = 'xz')
